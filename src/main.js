15304ff46c0c288a7a023e279a5b79a3a4949a45
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-04-01 14:05:02 +0200

    fix: code prettier

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 console.log("test");
 const body = document.body;
 const themeBtn = document.getElementById("themeBtn");
@@ -9,14 +9,14 @@ function themeSwitch(theme) {
 }
 
 themeBtn.addEventListener("click", () => {
-  const newTheme = body.classList.contains("dark-theme") ? "light-theme" : "dark-theme";
+  const newTheme = body.classList.contains("dark-theme")
+    ? "light-theme"
+    : "dark-theme";
   localStorage.setItem("theme", newTheme);
   themeSwitch(newTheme);
 });
 
 const savedTheme = localStorage.getItem("theme") || "light-theme";
 window.addEventListener("load", () => {
-    themeSwitch(savedTheme);
-}
-);
-
+  themeSwitch(savedTheme);
+});
